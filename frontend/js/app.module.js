ddcfd9a7e9b434c698fc3dfc70317403e507e387
Jason Land <wishinghand@gmail.com>
2016-09-10 18:04:08 -0700

    finished basic routes

--- frontend/js/app.module.js
+++ frontend/js/app.module.js
@@ -5,12 +5,46 @@
         .module('app', ['ui.router'])
         .config(appConfig);
 
-    appConfig.$inject= ['$urlRouterProvider', '$stateProvider']
+    appConfig.$inject= ['$urlRouterProvider', '$stateProvider'];
 
     	function appConfig($urlRouterProvider, $stateProvider){
     		$urlRouterProvider.otherwise('landing');
 
     		$stateProvider
-    			.state('landing')
+    			.state('404', {
+                    url: '/404', 
+                    controller: "404Ctrl as landing",
+                    templateUrl: 'landing/landing.html'
+                })
+                .state('browse', {
+                    url: '/browse', 
+                    controller: "BrowseCtrl as browse",
+                    templateUrl: 'browse/browse.html'
+                })
+                .state('game', {
+                    url: '/game', 
+                    controller: "GameCtrl as game",
+                    templateUrl: 'game/game.html'
+                })
+                .state('landing', {
+                    url: '/landing', 
+                    controller: "LandingCtrl as landing",
+                    templateUrl: 'landing/landing.html'
+                })
+                .state('profile', {
+                    url: '/profile', 
+                    controller: "ProfileCtrl as profile",
+                    templateUrl: 'profile/profile.html'
+                })
+                .state('signIn', {
+                    url: '/signIn', 
+                    controller: "SignInCtrl as signIn",
+                    templateUrl: 'signIn/signIn.html'
+                })
+                .state('signUp/', {
+                    url: '/signUp', 
+                    controller: "SignUpCtrl as signUp",
+                    templateUrl: 'signUp/signUp.html'
+                });
     	}
 })();
