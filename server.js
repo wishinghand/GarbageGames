1ae7eb5b2c19e6fcfffb70509d77b433bba70dcf
Joey sherman <joey.a.sherman@gmail.com>
2016-09-10 17:01:53 -0700

    Add: - nodemon - build folder - server.js to serve index.html from build folder

--- server.js
+++ server.js
@@ -3,11 +3,21 @@
  */
 
 var express = require('express');
-var app = express();
+var body_parser = require('body-parser');
+var path = require('path');
 
-app.use(express.static(path.join(__dirname, 'frontend/build')));
-app.listen('3000', function(){
-    
-}
-);
+var app = express();
+var port = '3000';
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(body_parser.json());
+
+/*app.get('*', function(req, res) {
+    res.sendFile('index.html');
+});*/
+
+app.listen(port, function(){
+    console.log('Server is running on: localhost:' + port);
+});
 
